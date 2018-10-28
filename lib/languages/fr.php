34eee654d71afd800d2bfddd8b280cd4f5a0bb83
Sylvain Julé <sylvain.jule@free.fr>
2018-10-28 10:13:58 +0100

    Add french translation

---
+++ lib/languages/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'subtitler.vttbuilder.update'     => 'Mettre à jour les .vtt',
+	'subtitler.vttbuilder.updated'    => 'Fichiers mis à jour !',
+	'subtitler.vttbuilder.create'     => 'Générer les .vtt',
+	'subtitler.vttbuilder.created'    => 'Fichiers créés !',
+	'subtitler.vttbuilder.error'      => 'Une erreur s\'est produite !',
+	'subtitler.vttbuilder.processing' => 'Traitement en cours…',
+);
