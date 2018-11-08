64ea0aa0cd14774de56e169d1642978c8540b258
jschopplich <27850750+jschopplich@users.noreply.github.com>
2018-11-08 10:46:16 +0100

    Add German translation

---
+++ lib/languages/de.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'subtitler.vttbuilder.update'     => 'Aktualisieren von .vtt-Dateien',
+	'subtitler.vttbuilder.updated'    => 'Dateien aktualisiert!',
+	'subtitler.vttbuilder.create'     => 'Erstellen von .vtt-Dateien',
+	'subtitler.vttbuilder.created'    => 'Dateien erstellt!',
+	'subtitler.vttbuilder.error'      => 'Es ist ein Fehler aufgetreten!',
+	'subtitler.vttbuilder.processing' => 'Verarbeiten…',
+);
